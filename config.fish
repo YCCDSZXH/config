a34f7f11a46d01cae5570b543bc7561cea87af2e
yl <yl@localhost.localdomain>
2023-04-16 00:42:10 +0800

    add alacritty history

--- config.fish
+++ config.fish
@@ -3,3 +3,25 @@ if status is-interactive
 end
 
 abbr -a pc proxychains4
+abbr -a gc git clone
+abbr -a cg cargo r
+
+# function add-env
+#     for i in $argv
+#         echo "ln $PWD/$i ~/.package/env-bin"
+#         ln $PWD/$i ~/.package/env-bin
+#     end
+# end
+
+# function add-bin
+#     for i in $argv
+#         echo "ln $PWD/$i ~/.package/env-bin"
+#         ln $PWD/$i ~/.package/bin
+#     end
+# end
+
+function VENV
+    source ~/.opt/share/VENV/bin/activate.fish
+end
+
+
